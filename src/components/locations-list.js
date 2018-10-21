e1572d9809ded3351983cde2163c3a12b932f24c
Abdu Assabri <abdusabri@outlook.com>
2018-10-21 16:14:29 +0200

    Display locations based on the json file

--- src/components/locations-list.js
+++ src/components/locations-list.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 
+import LOCATIONS from '../data/locations.json' 
+
 class LocationsList extends Component {
+
+    state = {
+        locations: LOCATIONS
+    }
 
     render() {
         return (
@@ -8,6 +14,15 @@ class LocationsList extends Component {
                 <input className='form-control form-control-lg' 
                     type='text' placeholder='Filter locations'
                     aria-label='Filter locations'/>
+                <ul className='list-group list-group-flush'>
+                    {this.state.locations.map((location) => (
+                        <li key={location.id}
+                            className="list-group-item list-group-item-action"
+                            tabIndex='0'>
+                            {location.name}
+                        </li>
+                    ))}
+                </ul>
             </article>
         )
     };
