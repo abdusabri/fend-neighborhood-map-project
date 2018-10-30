67d23b49f58724ba8b76e7bd05b91ea07419e807
Abdu Assabri <abdusabri@outlook.com>
2018-10-30 21:12:41 +0100

    Updates

---
+++ precache-manifest.6a278773ae7e04f2536f36eae328cb8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e807607324e16f960231",
+    "url": "/fend-neighborhood-map/static/js/runtime~main.e8076073.js"
+  },
+  {
+    "revision": "d1e6baa9dcb993c86795",
+    "url": "/fend-neighborhood-map/static/js/main.d1e6baa9.chunk.js"
+  },
+  {
+    "revision": "ffdca49d42e7f7e5915a",
+    "url": "/fend-neighborhood-map/static/js/1.ffdca49d.chunk.js"
+  },
+  {
+    "revision": "d1e6baa9dcb993c86795",
+    "url": "/fend-neighborhood-map/static/css/main.fef9bc0c.chunk.css"
+  },
+  {
+    "revision": "ffdca49d42e7f7e5915a",
+    "url": "/fend-neighborhood-map/static/css/1.5898628d.chunk.css"
+  },
+  {
+    "revision": "079e8143b0396378e43eb01e0cf745fd",
+    "url": "/fend-neighborhood-map/index.html"
+  }
+];
