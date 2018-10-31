d8f2a2600742f6bf0f4cc595e1ccdbe5c473ec30
Abdu Assabri <abdusabri@outlook.com>
2018-10-31 19:32:15 +0100

    Special handling for incorrect Mapbox API token

--- src/components/content-error.js
+++ src/components/content-error.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import { MdError } from 'react-icons/md';
+import PropTypes from 'prop-types';
 
-const ContentError = () => {
+const ContentError = (props) => {
     return (
         <div className='content-loader-container container-error'>
             <MdError className='error-icon text-warning'/>
             <div className='alert alert-warning alert-no-border text-center' role='alert'>
-                Oops! <br/> Something is not right :(
+                Oops! <br/>
+                {(props.errorMesage)? 
+                    props.errorMesage : 'Something is not right :('}
             </div>
         </div>
     )
+}
+
+ContentError.propTypes = {
+    errorMesage: PropTypes.string
+}
+
+ContentError.defaultProps = {
+    errorMesage: null
 }
 
 export default ContentError;
