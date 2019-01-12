464ae5c902a60f545559d22e8dce4e5b772c3a24
Abdu Assabri <abdusabri@outlook.com>
2019-01-12 15:03:40 +0100

    Updates

--- precache-manifest.d88da24bf9aba5ba17e8726c54c78b2b.js
+++ precache-manifest.d88da24bf9aba5ba17e8726c54c78b2b.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/fend-neighborhood-map-project/static/js/runtime~main.ac83a2dc.js"
   },
   {
-    "revision": "d948051cce9e84f58d39",
-    "url": "/fend-neighborhood-map-project/static/js/main.d948051c.chunk.js"
+    "revision": "4f1af8662b6bae53a163",
+    "url": "/fend-neighborhood-map-project/static/js/main.4f1af866.chunk.js"
   },
   {
     "revision": "1b5be6b5fa574d7bb594",
     "url": "/fend-neighborhood-map-project/static/js/1.1b5be6b5.chunk.js"
   },
   {
-    "revision": "d948051cce9e84f58d39",
+    "revision": "4f1af8662b6bae53a163",
     "url": "/fend-neighborhood-map-project/static/css/main.fef9bc0c.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/fend-neighborhood-map-project/static/css/1.5898628d.chunk.css"
   },
   {
-    "revision": "dce9c9a027ec5bbbb52127b94dff81f6",
+    "revision": "9fc42654be616552eb9fcdf2359c7c53",
     "url": "/fend-neighborhood-map-project/index.html"
   }
 ];
